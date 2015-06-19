86371ff0ace4e3e937fe901fe54adf928e8ae4e9
postvand <scot@motibox.com>
2015-06-19 13:54:17 -0600

    Fixed conflict merge error.

--- hsp-client/src/main/java/org/hspconsortium/client/auth/codeflow/CodeFlowAccessTokenRequest.java
+++ hsp-client/src/main/java/org/hspconsortium/client/auth/codeflow/CodeFlowAccessTokenRequest.java
@@ -8,7 +8,9 @@ import java.util.Map;
 
 public class CodeFlowAccessTokenRequest extends AbstractAccessTokenRequest {
 
-    public CodeFlowAccessTokenRequest(String clientId, String clientSecret) {
+    private Map<String, String> tokenRequestParams = new HashMap<String, String>();
+
+    public CodeFlowAccessTokenRequest(String clientId, String clientSecret, String code, String redirectUri) {
         super(clientId, clientSecret, AccessTokenGrantType.AUTHORIZATION_CODE);
         if (code != null) {
             this.tokenRequestParams.put("code", code);
