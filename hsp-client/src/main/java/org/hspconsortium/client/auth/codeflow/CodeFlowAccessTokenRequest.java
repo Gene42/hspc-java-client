914807ac00975b52ddefb3dee4889893c022370a
amy6 <amy@motibox.com>
2015-06-19 13:01:01 -0400

    CodeFlowAccessTokenRequest work.

--- hsp-client/src/main/java/org/hspconsortium/client/auth/codeflow/CodeFlowAccessTokenRequest.java
+++ hsp-client/src/main/java/org/hspconsortium/client/auth/codeflow/CodeFlowAccessTokenRequest.java
@@ -3,16 +3,24 @@ package org.hspconsortium.client.auth.codeflow;
 import org.hspconsortium.client.auth.AbstractAccessTokenRequest;
 import org.hspconsortium.client.auth.AccessTokenGrantType;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CodeFlowAccessTokenRequest extends AbstractAccessTokenRequest {
 
-    public CodeFlowAccessTokenRequest(String clientId, String clientSecret, AccessTokenGrantType grantType) {
+    private Map<String, String> tokenRequestParams = new HashMap<String, String>();
+
+    public CodeFlowAccessTokenRequest(String clientId, String clientSecret, String code, String redirectUri) {
         super(clientId, clientSecret, AccessTokenGrantType.AUTHORIZATION_CODE);
+        if (code != null) {
+            this.tokenRequestParams.put("code", code);
+        }
+        this.tokenRequestParams.put("redirect_uri", redirectUri);
+        this.tokenRequestParams.put("client_id", clientId);
     }
 
     @Override
     public Map<String, String> getAdditionalParameters() {
-        return null;
+        return this.tokenRequestParams;
     }
 }
